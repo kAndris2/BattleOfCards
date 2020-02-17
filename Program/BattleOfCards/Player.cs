939c1002913d20c85e0a6a1c10dbbe1117df4a4a
andri <andri@DESKTOP-FIORF47>
2020-02-17 12:57:23 +0100

    Add Player class

---
+++ Program/BattleOfCards/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleOfCards
+{
+    class Player
+    {
+        List<Card> Cards = new List<Card>();
+        String Name { get; set; }
+
+        public Player()
+        {
+        }
+
+        public List<Card> GetCards() { return Cards; }
+        public String GetName() { return Name;  }
+    }
+}
