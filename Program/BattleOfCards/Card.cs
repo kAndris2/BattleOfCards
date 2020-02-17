8e42e0bac05b97af98c03f681821e60e813e8c6f
andri <andri@DESKTOP-FIORF47>
2020-02-17 12:41:01 +0100

    add Card/RandomProperty class

---
+++ Program/BattleOfCards/Card.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleOfCards
+{
+    class Card
+    {
+        public string Name { get; private set; }
+        public double Acceleration { get; private set; }
+        public int Speed { get; private set; }
+        public int Weight { get; private set; }
+        public int Capacity { get; private set; }
+
+        public Card()
+        {
+            RandomProperty random = new RandomProperty();
+
+            Name = random.SetName();
+            Acceleration = random.SetAcceleration();
+            Speed = random.SetSpeed(); 
+            Capacity = random.SetCapacity();
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\n" +
+                   $"Acceleration: {Acceleration}\n" +
+                   $"Speed: {Speed}\n" +
+                   $"Weight: {Weight}\n" +
+                   $"Capacity: {Capacity}";
+        }
+    }
+}
