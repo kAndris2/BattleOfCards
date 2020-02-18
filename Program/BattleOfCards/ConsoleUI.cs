7f56812a1b9a877d5ff6152175f03ce72e19181c
burgonyapure <deejay.spory@gmail.com>
2020-02-18 13:06:17 +0100

    added consoleui.cs

---
+++ Program/BattleOfCards/ConsoleUI.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleOfCards
+{
+    class ConsoleUI
+    {
+        public void DisplayRound()
+        {
+
+        }
+
+        public void DisplayEndOfGame()
+        {
+            
+        }
+
+        public string PrintQuestion(string question)
+        {
+            string answer;
+            WriteGreen(question);
+            answer = Console.ReadLine();
+            return answer;
+        }
+
+        public void PrintError(string error)
+        {
+            WriteRed(error);
+        }
+        private void WriteGreen(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(value.PadRight(Console.WindowWidth - 1));
+            Console.ResetColor();
+        }
+
+        private void WriteRed(string value)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(value.PadRight(Console.WindowWidth - 1));
+            Console.ResetColor();
+        }
+
+
+    }
+}
