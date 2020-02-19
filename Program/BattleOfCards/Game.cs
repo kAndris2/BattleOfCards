7d1968de8f028686009f99dbeb26c62eb907e5d5
andri <andri@DESKTOP-FIORF47>
2020-02-19 12:21:13 +0100

    Start()++

--- Program/BattleOfCards/Game.cs
+++ Program/BattleOfCards/Game.cs
@@ -19,12 +19,45 @@ namespace BattleOfCards
         public void Start()
         {
             Table = new Table();
-            int num = int.Parse(Display.PrintQuestion("How many players are involved in the game?"));
+            int num, bot_count;
 
-            if (num <= 1 || num > 8)
-                throw new ArgumentException("Too much player, Max 8!");
+            while (true)
+            {
+                num = int.Parse(Display.PrintQuestion("How many players are involved in the game?"));
+
+                if (num <= 1 || num > 8)
+                {
+                    Display.PrintError("Too much player, Max 8!");
+                    continue;
+                }
+
+                bot_count = int.Parse(Display.PrintQuestion("How many bot participate in the game?"));
+
+                if (bot_count > num)
+                {
+                    Display.PrintError("Bot count is greater than player count!");
+                    continue;
+                }
+                break;
+            }
 
             GInit = new GameInit(num);
+            GInit.CreateDeck();
+
+            RandomProperty random = new RandomProperty();
+
+            for (int i = 0; i < num; i++)
+            {
+                if (bot_count > 0)
+                {
+                    GInit.CreatePlayer(new Bot(Display.PrintQuestion("BOT" + i), random.SetID()));
+                    bot_count--;
+                }
+                else
+                    GInit.CreatePlayer(new Human(Display.PrintQuestion("Enter your name:"), random.SetID()));
+            }
+
+            GInit.DealCards();
             StarterPlayer = GInit.GetPlayers()[0];
 
             while (!CheckWinner())
