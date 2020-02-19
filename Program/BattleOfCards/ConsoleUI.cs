e9bba6b8f8fe1efd6f39996ce654205f8ea62f54
burgonyapure <deejay.spory@gmail.com>
2020-02-19 15:00:02 +0100

    Added Diplay.cs and modified ConsoleUI.cs

--- Program/BattleOfCards/ConsoleUI.cs
+++ Program/BattleOfCards/ConsoleUI.cs
@@ -9,8 +9,18 @@ namespace BattleOfCards
     {
         public void DisplayRound(Player StarterPlayer)
         {
-            Console.WriteLine(StarterPlayer.GetCards().GetTopCard().ToString());
-            //PrintQuestion("Wich property would u wanna play with?");
+            string detail = StarterPlayer.GetCards().GetTopCard().ToString();
+            string[] a = detail.Split('\n');
+            Diplay.PrintLine();
+            Diplay.PrintRow(a[0]);
+            Diplay.PrintRow(a[1]);
+            Diplay.PrintRow(a[2]);
+            Diplay.PrintRow(a[3]);
+            Diplay.PrintRow(a[4]);
+            Diplay.PrintRow(a[5]);
+            Diplay.PrintRow(a[6]);
+            Diplay.PrintLine();
+            
         }
 
         public void DisplayEndOfGame(Player player)
