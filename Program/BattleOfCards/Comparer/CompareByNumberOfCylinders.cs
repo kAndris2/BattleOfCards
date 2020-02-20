3ba819c4da17e916ee663c816e0ffbcb90599eb6
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-02-20 09:30:35 +0100

    add compare by cylinder

---
+++ Program/BattleOfCards/Comparer/CompareByNumberOfCylinders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BattleOfCards
+{
+    class CompareByNumberOfCylinders : IComparer<Player>
+    {
+        public int Compare(Player x, Player y)
+        {
+            if (x.GetCards().GetTopCard().Cylinder == y.GetCards().GetTopCard().Cylinder)
+            {
+                return 0;
+            }
+            if (x.GetCards().GetTopCard().Cylinder < y.GetCards().GetTopCard().Cylinder)
+            {
+                return 1;
+            }
+            return -1;
+        }
+    }
+}
