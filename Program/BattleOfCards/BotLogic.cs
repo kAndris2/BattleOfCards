6e2ba16df01f7286e17bde169bf2cc294cc12ae3
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-02-21 09:21:30 +0100

    fixing cylinder chance

--- Program/BattleOfCards/BotLogic.cs
+++ Program/BattleOfCards/BotLogic.cs
@@ -18,7 +18,7 @@ namespace BattleOfCards
             Dictionary<string, double> stat = deck.cardstat.GetStat();
             Dictionary<string, double> chance = new Dictionary<string, double>();
             chance.Add("Capacity" , (card.Capacity - stat["Min_Capacity"]) / (stat["Max_Capacity"] - stat["Min_Capacity"])*100);
-            chance.Add("Cylinder", 100-((card.Cylinder - stat["Min_Cylinder"]) / (stat["Max_Cylinder"] - stat["Min_Cylinder"])) * 100);
+            chance.Add("Cylinder", (card.Cylinder - stat["Min_Cylinder"]) / (stat["Max_Cylinder"] - stat["Min_Cylinder"]) * 100);
             chance.Add("Weight", 100-((card.Weight - stat["Min_Weight"]) / (stat["Max_Weight"] - stat["Min_Weight"])) * 100);
             chance.Add("Acceleration", 80-((card.Acceleration - stat["Min_Acceleration"]) / (stat["Max_Acceleration"] - stat["Min_Acceleration"])) * 100);
             chance.Add("HP", (card.HP - stat["Min_HP"]) / (stat["Max_HP"] - stat["Min_HP"]) * 100);
